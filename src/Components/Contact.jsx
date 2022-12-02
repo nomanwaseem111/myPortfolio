a995966f346118102705ae5bacea7a8b56e6347c
Noman Waseem <nomanwaseem111@gmail.com>
2022-12-02 05:54:10 +0500

    again changes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,80 +1,149 @@
-import React from 'react'
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-const Contact = () => {
- 
- 
-  const [inputValue,setInputValue] = useState({
-    fname:'',
-    lname:'',
-    email:'',
-    number:'',
-    message:''
-  })
- 
-  console.log(inputValue);
- 
-   const getValue = (e) => {
- 
-     const {name,value} = e.target
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import TextareaAutosize from '@mui/base/TextareaAutosize';
+import axios from 'axios'
 
-     setInputValue(() => {
-       return {
-          ...inputValue,
-          [name]:value
-       }
-     })
 
-  }
-
+function Copyright(props: any) {
   return (
-    <>
-      <div className='container'>
-        <h2 className='text-center mt-4'>Contact us</h2>
-        <div className='container mt-4'>
-          <Form className='row mt-4 mb-5'>
-            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter First Name" name="fname" onChange={getValue} required />
-
-            </Form.Group>
-            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter Last Name" name="lname" onChange={getValue} required />
-
-            </Form.Group>
-
-            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Enter Email" name="email" onChange={getValue} required />
-
-            </Form.Group>
-
-            <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
-              <Form.Label>Mobile</Form.Label>
-              <Form.Control type="number" placeholder="Enter Number" name="number" onChange={getValue} required />
-
-            </Form.Group>
-
-            <Form.Group className="mb-3 col-lg-12" controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Message</Form.Label>
-              <Form.Control as="textarea" rows={3} name="message" placeholder="Message....." onChange={getValue} required />
-            </Form.Group>
-
-            <div className='d-flex justify-content-center'>
-
-              <Button variant="primary col-lg-6" type="submit">
-                Submit
-              </Button>
-            </div>
-
-
-          </Form>
-        </div>
-      </div>
-    </>
-  )
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Contact
+const theme = createTheme();
+
+export default function SignIn() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      firstName: data.get('firstName'),
+      lastName: data.get('lastName'),
+      email: data.get('email'),
+      number: data.get('number'),
+      message: data.get('message'),
+    });
+
+    axios.post('http:localhost:5002/submit', {
+      firstName: data.get('firstName'),
+      lastName: data.get('lastName'),
+      email: data.get('email'),
+      number: data.get('number'),
+      message: data.get('message'),
+
+    })
+    .then(function (response) {
+      console.log(response.data.message);
+    })
+    .catch(function (error) {
+      console.log(error.response.data.message);
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Contact Form
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="firstName"
+              label="First Name"
+              name="firstName"
+              autoComplete="firstName"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="lastName"
+              label="Last Name"
+              name="lastName"
+              autoComplete="lastName"
+              autoFocus
+            />
+
+
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="number"
+              label="Number"
+              type="number"
+              id="number"
+            />
+            <TextareaAutosize
+              maxRows={200}
+              aria-label="maximum height"
+              placeholder="Message"
+              name="message"
+              id="message"
+
+              defaultValue=""
+              style={{ width: 400, height: 200, padding: 10 }}
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Submit
+            </Button>
+
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
